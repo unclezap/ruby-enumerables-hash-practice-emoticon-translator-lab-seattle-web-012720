299f2c9e9e84c27e91f87f90ea2ef6f502bddcd3
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2020-01-24 01:08:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,16 @@
 # require modules here
 
-def load_library
+def load_library(emoticons)
   # code goes here
+  
+  require 'yaml'
+  data = YAML.load(File.open('emoticons.yaml'))
+      
+  value = {:get_meaning => meaning, :get_emoticon => emote}
+  return value
 end
+
+
 
 def get_japanese_emoticon
   # code goes here
